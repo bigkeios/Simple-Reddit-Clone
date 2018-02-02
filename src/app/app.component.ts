afdadf61908d3de9ce66944795042377dfc5bf27
Phuong C <cqphuong97@gmail.com>
2018-02-02 16:12:09 +0700

    Added articles

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -5,6 +5,12 @@ import { Component } from '@angular/core';
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-  title = 'app';
+export class AppComponent 
+{
+  addArticle(titleInput: HTMLInputElement,linkInput: HTMLInputElement):boolean
+  {
+    console.log(`Adding article ${linkInput.value} with ${titleInput.value} `);
+    // use the id, not the name of the variable they are turned into
+    return false;
+  }
 }
